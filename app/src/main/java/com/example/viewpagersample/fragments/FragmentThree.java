3c611594a1cae862a3b1289e3e118107916409d0
Parth <parth1493@gmail.com>
2019-09-02 14:53:46 -0400

    Added simple view pager

--- app/src/main/java/com/example/viewpagersample/fragments/FragmentThree.java
+++ app/src/main/java/com/example/viewpagersample/fragments/FragmentThree.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.sriyanksiddhartha.fragmentpageradapter.R;
+import com.example.viewpagersample.R;
 
 public class FragmentThree extends Fragment {
 
--- app/src/main/java/com/example/viewpagersample/fragments/FragmentTwo.java
+++ app/src/main/java/com/example/viewpagersample/fragments/FragmentTwo.java
@@ -7,7 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.sriyanksiddhartha.fragmentpageradapter.R;
+import com.example.viewpagersample.R;
+
 
 public class FragmentTwo extends Fragment {
 
